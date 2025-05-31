a52abdc3f004125bb2e6a762152d326a898b9c17
Radu <radu.avadanii@gmail.com>
2025-05-31 17:28:56 +0300

    upd: project routes.

---
+++ server/src/index.ts
@@ -0,0 +1,35 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+/* ROUTE IMPORTS */
+import projectRoutes from "./routes/projectRoutes";
+
+/* CONFIGURATION */
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+/* ROUTES */
+app.get("/", (req, res) => {
+    res.status(200).send("Welcome to the API!");
+});
+
+app.use("/projects", projectRoutes);
+
+/* SERVER */
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+}).on("error", (err) => {
+    console.error(`Error starting server: ${err.message}`);
+    process.exit(1);
+});
