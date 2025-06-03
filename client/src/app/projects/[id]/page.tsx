171d655b24d72ea5f5d8213ad11d066abb4eb31f
Radu <radu.avadanii@gmail.com>
2025-06-03 18:14:48 +0300

    upd: board view.

---
+++ client/src/app/projects/[id]/page.tsx
@@ -0,0 +1,46 @@
+"use client";
+
+import React, { use, useState } from "react";
+import ProjectHeader from "@/app/projects/ProjectHeader";
+import Board from "../BoardView";
+// import List from "../ListView";
+// import Timeline from "../TimelineView";
+// import Table from "../TableView";
+// import ModalNewTask from "@/components/ModalNewTask";
+
+type Props = {
+  params: Promise<{ id: string }>;
+};
+
+const Project = ({ params }: Props) => {
+  const { id } = use(params);
+  console.log("Project ID:", id);
+
+  const [activeTab, setActiveTab] = useState("Board");
+  const [isModalNewTaskOpen, setIsModalNewTaskOpen] = useState(false);
+
+  return (
+    <div>
+      {/* <ModalNewTask
+        isOpen={isModalNewTaskOpen}
+        onClose={() => setIsModalNewTaskOpen(false)}
+        id={id}
+      /> */}
+      <ProjectHeader activeTab={activeTab} setActiveTab={setActiveTab} />
+      {activeTab === "Board" && (
+        <Board id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {/*  {activeTab === "List" && (
+        <List id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Timeline" && (
+        <Timeline id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )}
+      {activeTab === "Table" && (
+        <Table id={id} setIsModalNewTaskOpen={setIsModalNewTaskOpen} />
+      )} */}
+    </div>
+  );
+};
+
+export default Project;
--- client/src/components/Sidebar/index.tsx
+++ client/src/components/Sidebar/index.tsx
@@ -2,6 +2,7 @@
 
 import { useAppDispatch, useAppSelector } from "@/app/redux";
 import { setIsSidebarCollapsed } from "@/state";
+import { useGetProjectsQuery } from "@/state/api";
 import {
   AlertCircle,
   AlertOctagon,
@@ -29,6 +30,7 @@ const Sidebar = () => {
   const [showProjects, setShowProjects] = useState(true);
   const [showPriority, setShowPriority] = useState(true);
 
+  const { data: projects } = useGetProjectsQuery();
   const dispatch = useAppDispatch();
   const isSidebarCollapsed = useAppSelector(
     (state) => state.global.isSidebarCollapsed,
@@ -92,7 +94,7 @@ const Sidebar = () => {
           )}
         </button>
         {/* PROJECTS LIST */}
-        {/* {showProjects &&
+        {showProjects &&
           projects?.map((project) => (
             <SidebarLink
               key={project.id}
@@ -100,7 +102,7 @@ const Sidebar = () => {
               label={project.name}
               href={`/projects/${project.id}`}
             />
-          ))} */}
+          ))}
         {/* PRIORITIES LINKS */}
         <button
           onClick={() => setShowPriority((prev) => !prev)}
